96d46bd6ab56c6a821015912d0e5e661b678a64b
Jagrati Joshi <jagrati.joshi@outlook.com>
2020-12-30 00:20:08 -0700

    Updated Dropdown

--- Visualizations-Dashboards/static/js/app.js
+++ Visualizations-Dashboards/static/js/app.js
@@ -1,43 +1,79 @@
+var selectedID;
+
+function init() {
+    loadIDs();
+    barPlot();
+}
+
+function loadIDs() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    d3.json("data/samples.json").then((importedData) => {
+        ids = importedData.names;
+        ids.forEach((id) => {
+            if (selectedID === null) {
+                selectedID = id ;
+            }
+            var option = dropdownMenu.append("option");
+            option.text(id);
+        });
+    });
+}
+
+function optionChanged(selectedValue) {
+    selectedID = selectedValue;
+    barPlot();
+}
 // Use d3.json() to fetch data from JSON file
 // Incoming data is internally referred to as importedData
-d3.json("data/samples.json").then((importedData) => {
-    var data = importedData.samples;
-  
-    // Sort the data array using the greekSearchResults value
-    data.sort(function(a, b) {
-        return parseFloat(b.sample_values) - parseFloat(a.sample_values);
+function barPlot() {
+    d3.json("data/samples.json").then((importedData) => {
+        var data = importedData.samples;
+        
+        var filteredData = filterData(data);
+
+        // Sort the data array using the greekSearchResults value
+        filteredData.sort(function(a, b) {
+            return parseFloat(b.sample_values) - parseFloat(a.sample_values);
+        });
+
+        // Slice the first 10 objects for plotting
+        filteredData = filteredData.slice(0, 10);
+
+        // Reverse the array due to Plotly's defaults
+        filteredData = filteredData.reverse();
+    
+        // Create your trace.
+        var trace = {
+            x: filteredData.map(row => row.sample_values),
+            y: filteredData.map(row => row.otu_ids),
+            text: filteredData.map(row => row.otu_labels),
+            name: "OTU",
+            type: "bar",
+            orientation: "h"
+        };
+    
+        // Create the data array for our plot
+        var chartData = [trace];
+    
+        // Define the plot layout
+        var layout = {
+            title: "Top Ten OTUs",
+            margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+            }
+        };
+    
+        // Plot the chart to a div tag with id "bar-plot"
+        Plotly.newPlot("bar", chartData, layout);
     });
+}
 
-    // Slice the first 10 objects for plotting
-    data = data.slice(0, 10);
+function filterData(data) {
+    return data.id = selectedID;
+}
 
-    // Reverse the array due to Plotly's defaults
-    data = data.reverse();
-  
-    // Create your trace.
-    var trace = {
-        x: data.map(row => row.sample_values),
-        y: data.map(row => row.otu_ids),
-        text: data.map(row => row.otu_labels),
-        name: "OTU",
-        type: "bar",
-        orientation: "h"
-    };
-  
-    // Create the data array for our plot
-    var chartData = [trace];
-  
-    // Define the plot layout
-    var layout = {
-        title: "Top Ten OTUs",
-        margin: {
-          l: 100,
-          r: 100,
-          t: 100,
-          b: 100
-        }
-    };
-  
-    // Plot the chart to a div tag with id "bar-plot"
-    Plotly.newPlot("bar", chartData, layout);
-});
+init();
